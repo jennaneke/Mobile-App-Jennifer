cfe17e2b5aa72e3cc9db6c176226e15e9e831049
Jennifer <jenaneke@yahoo.com>
2015-09-22 23:30:29 -0400

    Create myapp.js

---
+++ myapp.js
@@ -0,0 +1,10 @@
+# Mobile-App-Jennifer
+var myApp = angular.module('myApp', ['ionic']);
+
+myApp.controller('name-two', function($scope){
+ $scope.Jennifer="Heyy Jennifer";
+ $scope.items = (["name": "ipad1", "ipad2", "Mac laptop", "room": "405", "406", "407"])
+
+ }
+);
+
